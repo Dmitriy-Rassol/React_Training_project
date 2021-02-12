535a5f84e916c26331f27d7b006ee737ad595ea2
Dmitriy <rassoldima@yandex.ru>
2021-02-13 02:35:01 +0300

    добавил допы для продуктов

--- src/Components/Modal/ModalItem.js
+++ src/Components/Modal/ModalItem.js
@@ -5,6 +5,8 @@ import { CountItem } from './CountItem';
 import { useCount } from '../Hooks/UseCount';
 import {TotalPriceItems} from '../Functions/secondaryFunction';
 import {formatCurrency} from '../Functions/secondaryFunction';
+import { Toppings } from './Toppings';
+import { useToppings } from '../Hooks/useToppings';
 
 const Overlay = styled.div `
     position: fixed;
@@ -56,6 +58,7 @@ const TotalPriceItem = styled.div `
 export const ModalItem = ({openItem, setOpenItem, orders, setOrders}) => {
 
     const counter = useCount();
+    const toppings = useToppings(openItem);
 
     const closeModal = (e) => {
         if(e.target.id === "overlay") {
@@ -65,7 +68,8 @@ export const ModalItem = ({openItem, setOpenItem, orders, setOrders}) => {
 
     const order = {
         ...openItem,
-        count: counter.count
+        count: counter.count,
+        topping: toppings.toppings
     };
 
     const addToOrder = () => {
@@ -84,11 +88,11 @@ export const ModalItem = ({openItem, setOpenItem, orders, setOrders}) => {
                             {openItem.name}
                         </div>
                         <div>
-                            {openItem.price.toLocaleString('ru-RU',
-                            {style: 'currency', currency: 'RUB'})}
+                            {formatCurrency(openItem.price)}
                         </div>
                     </HeaderContent>
                     <CountItem {...counter}/>
+                    {openItem.toppings &&  <Toppings {...toppings}/>}
                     <TotalPriceItem>
                         <span>
                             Цена:
